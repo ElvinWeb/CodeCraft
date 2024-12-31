3a7115d922631dd6dda31568ba7d4a32e711c35c
Elvin <elvinsrkrov@gmail.com>
2024-12-31 11:33:50 +0400

    Editor panel UI has created

---
+++ src/components/OutputPanel.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+function OutputPanel() {
+  return (
+    <div className="relative bg-[#181825] rounded-xl p-4 ring-1 ring-gray-800/50"></div>
+  );
+}
+
+export default OutputPanel;
